8d09d36a1e75f0c3ae96855f3e7deb92805525ac
KNLNT <apovolnov@gmail.com>
2019-08-09 11:58:08 +0500

    asdd

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 const debug = process.env.NODE_ENV !== "production";
+const assetPrefix =!debug ? 'https://knlnt.github.io/dotaAPI/out/' : '';
+const webpack = require('webpack');
 
 module.exports = {
   exportPathMap: function() {
     return {
-      '/dotaAPI/out/': { page: '/' }
+      '/': { page: '/' }
     };
   },
-  assetPrefix: !debug ? 'https://knlnt.github.io/dotaAPI/out/' : '',
+  assetPrefix: assetPrefix,
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    )
+
+    return config
+  }
 };
